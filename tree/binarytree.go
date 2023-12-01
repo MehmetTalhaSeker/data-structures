8931360a79ade2fd7a4f970f26c9cfdcc86a22dc
mehmettalhaseker <mtalhaseker22@gmail.com>
2023-12-01 11:15:09 +0300

    Binary Tree and tests done.

--- tree/binarytree.go
+++ tree/binarytree.go
@@ -2,6 +2,7 @@ package tree
 
 import (
 	"data-structures/stack"
+	"slices"
 )
 
 type Node[T any] struct {
@@ -51,4 +52,99 @@ func (b *BinaryTree[T]) PreOrderTraversal() []T {
 	}
 
 	return a
+}
+
+func (b *BinaryTree[T]) PostOrderTraversal() []T {
+	if b.Root == nil {
+		return nil
+	}
+
+	s := stack.NewStack(make([]Node[T], 0))
+	s.Push(*b.Root)
+
+	result := make([]T, 0)
+
+	for {
+		_, err := s.Peek()
+		if err != nil {
+			break
+		}
+
+		curr, err := s.Pop()
+		if err == nil {
+			result = append(result, curr.Value)
+		}
+
+		if curr.Left != nil {
+			s.Push(*curr.Left)
+		}
+
+		if curr.Right != nil {
+			s.Push(*curr.Right)
+		}
+
+	}
+
+	slices.Reverse(result)
+
+	return result
+}
+
+func (b *BinaryTree[T]) InOrderTraversal() []T {
+	if b.Root == nil {
+		return nil
+	}
+
+	s := stack.NewStack(make([]Node[T], 0))
+
+	result := make([]T, 0)
+
+	curr := b.Root
+
+	for {
+		_, err := s.Peek()
+		if err != nil && curr == nil {
+			break
+		}
+
+		for curr != nil {
+			s.Push(*curr)
+			curr = curr.Left
+		}
+
+		curr, err = s.Pop()
+		if err != nil {
+			break
+		}
+
+		result = append(result, curr.Value)
+
+		curr = curr.Right
+	}
+
+	return result
+}
+
+func PreOrderTraversalRecursive[T any](r *Node[T]) []T {
+	if r == nil {
+		return []T{}
+	}
+
+	return append([]T{r.Value}, append(PreOrderTraversalRecursive(r.Left), PreOrderTraversalRecursive(r.Right)...)...)
+}
+
+func PostOrderTraversalRecursive[T any](r *Node[T]) []T {
+	if r == nil {
+		return []T{}
+	}
+
+	return append(PostOrderTraversalRecursive(r.Left), append(PostOrderTraversalRecursive(r.Right), r.Value)...)
+}
+
+func InOrderTraversalRecursive[T any](r *Node[T]) []T {
+	if r == nil {
+		return []T{}
+	}
+
+	return append(InOrderTraversalRecursive(r.Left), append([]T{r.Value}, InOrderTraversalRecursive(r.Right)...)...)
 }
--- tree/binarytree_test.go
+++ tree/binarytree_test.go
@@ -11,43 +11,158 @@ var _ = Describe("BinaryTree", func() {
 	var (
 		bt tree.BinaryTree[int]
 		nn *tree.Node[int]
+		rn *tree.Node[int]
 	)
 
-	BeforeEach(func() {
-		root := &tree.Node[int]{Value: 1}
-		root.Left = &tree.Node[int]{Value: 2}
-		root.Right = &tree.Node[int]{Value: 3}
-		root.Left.Left = &tree.Node[int]{Value: 4}
-		root.Left.Right = &tree.Node[int]{Value: 5}
+	Describe("Non recursive", func() {
+		BeforeEach(func() {
+			root := &tree.Node[int]{Value: 1}
+			root.Left = &tree.Node[int]{Value: 2}
+			root.Right = &tree.Node[int]{Value: 3}
 
-		bt = *tree.NewBinaryTree[int](root)
-	})
+			root.Left.Left = &tree.Node[int]{Value: 4}
+			root.Left.Right = &tree.Node[int]{Value: 5}
 
-	Describe("PreOrderTraversal", func() {
-		Context("when the tree is not empty", func() {
-			It("should return elements in pre-order traversal order", func() {
-				result := bt.PreOrderTraversal()
-				Expect(result).To(Equal([]int{1, 2, 4, 5, 3}))
-			})
-			It("should return elements in pre-order traversal order", func() {
-				bt.Root.Left.Left.Left = &tree.Node[int]{Value: 555}
+			root.Right.Left = &tree.Node[int]{Value: 6}
+			root.Right.Right = &tree.Node[int]{Value: 7}
 
-				result := bt.PreOrderTraversal()
-				Expect(result).To(Equal([]int{1, 2, 4, 555, 5, 3}))
-			})
+			bt = *tree.NewBinaryTree[int](root)
 		})
 
-		Context("when the tree is empty", func() {
-			BeforeEach(func() {
-				bt = *tree.NewBinaryTree(nn)
+		Describe("PreOrderTraversal", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in pre-order traversal order", func() {
+					result := bt.PreOrderTraversal()
+					Expect(result).To(Equal([]int{1, 2, 4, 5, 3, 6, 7}))
+				})
+				It("should return elements in pre-order traversal order", func() {
+					bt.Root.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := bt.PreOrderTraversal()
+					Expect(result).To(Equal([]int{1, 2, 4, 555, 5, 3, 6, 7}))
+				})
 			})
 
-			It("should return nil", func() {
-				result := bt.PreOrderTraversal()
-				Expect(result).To(BeNil())
+			Context("when the tree is empty", func() {
+				BeforeEach(func() {
+					bt = *tree.NewBinaryTree(nn)
+				})
+
+				It("should return nil", func() {
+					result := bt.PreOrderTraversal()
+					Expect(result).To(BeNil())
+				})
+			})
+		})
+		Describe("PostOrderTraversal", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in post-order traversal order", func() {
+					result := bt.PostOrderTraversal()
+					Expect(result).To(Equal([]int{4, 5, 2, 6, 7, 3, 1}))
+				})
+				It("should return elements in post-order traversal order", func() {
+					bt.Root.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := bt.PostOrderTraversal()
+					Expect(result).To(Equal([]int{555, 4, 5, 2, 6, 7, 3, 1}))
+				})
+			})
+
+			Context("when the tree is empty", func() {
+				BeforeEach(func() {
+					bt = *tree.NewBinaryTree(nn)
+				})
+
+				It("should return nil", func() {
+					result := bt.PostOrderTraversal()
+					Expect(result).To(BeNil())
+				})
+			})
+		})
+		Describe("InOrderTraversal", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in in-order traversal order", func() {
+					result := bt.InOrderTraversal()
+					Expect(result).To(Equal([]int{4, 2, 5, 1, 6, 3, 7}))
+				})
+				It("should return elements in in-order traversal order", func() {
+					bt.Root.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := bt.InOrderTraversal()
+					Expect(result).To(Equal([]int{555, 4, 2, 5, 1, 6, 3, 7}))
+				})
+			})
+
+			Context("when the tree is empty", func() {
+				BeforeEach(func() {
+					bt = *tree.NewBinaryTree(nn)
+				})
+
+				It("should return nil", func() {
+					result := bt.InOrderTraversal()
+					Expect(result).To(BeNil())
+				})
 			})
 		})
 	})
+
+	Describe("recursive", func() {
+		BeforeEach(func() {
+			rn = &tree.Node[int]{Value: 1}
+			rn.Left = &tree.Node[int]{Value: 2}
+			rn.Right = &tree.Node[int]{Value: 3}
+
+			rn.Left.Left = &tree.Node[int]{Value: 4}
+			rn.Left.Right = &tree.Node[int]{Value: 5}
+
+			rn.Right.Left = &tree.Node[int]{Value: 6}
+			rn.Right.Right = &tree.Node[int]{Value: 7}
+		})
+
+		Describe("PreOrderTraversal Recursive", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in pre-order traversal order", func() {
+					result := tree.PreOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{1, 2, 4, 5, 3, 6, 7}))
+				})
+				It("should return elements in pre-order traversal order", func() {
+					rn.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := tree.PreOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{1, 2, 4, 555, 5, 3, 6, 7}))
+				})
+			})
+		})
+		Describe("PostOrderTraversal Recursive", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in post-order traversal order", func() {
+					result := tree.PostOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{4, 5, 2, 6, 7, 3, 1}))
+				})
+				It("should return elements in post-order traversal order", func() {
+					rn.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := tree.PostOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{555, 4, 5, 2, 6, 7, 3, 1}))
+				})
+			})
+		})
+		Describe("InOrderTraversal Recursive", func() {
+			Context("when the tree is not empty", func() {
+				It("should return elements in in-order traversal order", func() {
+					result := tree.InOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{4, 2, 5, 1, 6, 3, 7}))
+				})
+				It("should return elements in in-order traversal order", func() {
+					rn.Left.Left.Left = &tree.Node[int]{Value: 555}
+
+					result := tree.InOrderTraversalRecursive(rn)
+					Expect(result).To(Equal([]int{555, 4, 2, 5, 1, 6, 3, 7}))
+				})
+			})
+		})
+	})
+
 })
 
 func TestBinaryTree(t *testing.T) {
